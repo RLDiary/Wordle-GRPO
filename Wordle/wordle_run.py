0314be438dabe080b23b6a4277e14589a0a360e0
Vignesh Ramesh <vigneshbabu.ram@gmail.com>
2025-06-29 18:36:31 +0000

    Further Refined

--- Wordle/wordle_run.py
+++ Wordle/wordle_run.py
@@ -13,26 +13,23 @@ def main():
     training_args.report_to = 'none'
     training_args.use_vllm = True
     training_args.num_iterations=1
-    training_args.per_device_train_batch_size=6
+    training_args.per_device_train_batch_size=3
     training_args.num_generations=12
-    training_args.gradient_accumulation_steps=4
     training_args.max_prompt_length=1024
     training_args.max_completion_length=4096
     training_args.max_steps=100
     training_args.vllm_mode = 'colocate'
     training_args.vllm_gpu_memory_utilization = 0.3
     training_args.vllm_tensor_parallel_size = 1
+    training_args.per_device_train_batch_size = 1
+    training_args.per_device_eval_batch_size = 1
     training_args.use_liger_loss = True
     rubric = W.WordleRubric()
     reward_funcs = rubric.get_reward_funcs()
     training_args.reward_weights = rubric.get_reward_weights()
 
     training_args.generation_kwargs = {
-        "temperature": 0.0,
-        "top_p": 0.95,
-        "top_k": 40,
-        "repetition_penalty": 1.0,
-        "min_p": 0.0,
+        "temperature": 1.0
     }
 
     trainer = W.GRPOMultiTurnTrainer(
